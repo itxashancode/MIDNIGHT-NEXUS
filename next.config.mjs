9a77e789a9762f8710caf13ca870fa366ebd69f5
Ashan Dev <234882555+itxashancode@users.noreply.github.com>
2026-05-16 10:13:10 +0500

    feat: add PWA manifest and update CSP to include Google Analytics and Tag Manager domains

--- next.config.mjs
+++ next.config.mjs
@@ -22,7 +22,7 @@ const nextConfig = {
           },
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https:; font-src 'self' data:; connect-src 'self' https: wss:;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https:; font-src 'self' data:; connect-src 'self' https: wss: https://www.google-analytics.com https://*.google-analytics.com;",
           },
         ],
       },
